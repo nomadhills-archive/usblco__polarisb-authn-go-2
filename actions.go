845aa23beccc8a275d5a41b8d28abf8b404382db
nomadhills <75098873+nomadhills@users.noreply.github.com>
2022-12-14 09:26:16 -0500

    exposed middleware

--- actions.go
+++ actions.go
@@ -1,10 +1,15 @@
 package polarisb_authn_go_2
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
 	"github.com/usblco/polarisb-authn-go-2/pkg"
 )
 
 func (p *PolarisbNativeAuthn) ValidateAccessToken(accessTokenString string) (refreshToken *jwt.Token, state pkg.ResultState, err error) {
 	return p.internal.Actions.AccessTokenValidate(accessTokenString)
+}
+
+func (p *PolarisbNativeAuthn) AuthenticateMiddleware() gin.HandlerFunc {
+	return p.internal.Middleware.Authenticate()
 }
